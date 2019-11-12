4b048e8bd15d1e1db25aa046312ab4e23709f12a
DanyZy <daniilzgurovec@gmail.com>
2019-11-12 23:21:21 +0600

    implemented way to render hexagon-shape platform

---
+++ Assets/Scripts/HexCellRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
+public class HexCellRenderer : MonoBehaviour
+{
+    Mesh hexMesh;
+
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+        hexMesh.name = "Hex Mesh";
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+    }
+
+    void Start()
+    {
+        Triangulate();
+
+        GetComponent<MeshCollider>().sharedMesh = hexMesh;
+    }
+
+    void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
+    {
+        int vertexIndex = vertices.Count;
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+    }
+
+    void Triangulate()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            AddTriangle(
+                Vector3.zero,
+                Vector3.zero + HexMetrics.corners[i],
+                Vector3.zero + HexMetrics.corners[i + 1]
+            );
+        }
+        hexMesh.vertices = vertices.ToArray();
+        hexMesh.triangles = triangles.ToArray();
+    }
+}
