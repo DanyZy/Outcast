55fcc55a5ba1a728a9965b084cc8a734f0e5fd29
DanyZy <daniilzgurovec@gmail.com>
2019-11-19 02:03:28 +0600

    implemented Pooler

--- Assets/Scripts/GroundTiling.cs
+++ Assets/Scripts/GroundTiling.cs
@@ -19,6 +19,9 @@ public class GroundTiling : MonoBehaviour
         //Initional platform
         Instantiate(platformPrefab, startPosition, Quaternion.identity, parentGround);
 
+        Instantiate(platformPrefab, startPosition + new Vector3(100, 0, 0), Quaternion.identity, parentGround);
+
+
         playerCCS.onCollisionEnterFunction = CreateNewPlatform;
     }
 
---
+++ Assets/Scripts/Pooler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public int size;
+        public GameObject prefab;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for(int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+}
